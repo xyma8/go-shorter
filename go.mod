2b4989a210aeea47e3eb87969807b7af7ad65b97
Nikita <semenovknikita@gmail.com>
2026-02-01 11:55:44 +0300

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xyma8/go-shorter
+
+go 1.25.6
--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
+type Message struct {
+	Original_url string
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello")
+	//fmt.Println(*req)
+	//body := req.Body
+	//body, _ := req.GetBody()
+	fmt.Println(req.Body)
+	json_decoder := json.NewDecoder(req.Body)
+
+	var body_content Message
+	err := json_decoder.Decode(&body_content)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(body_content.Original_url)
+	//longBuf := make([]byte, 1)
+	//fmt.Println(body.Read(longBuf))
+
+	//if _, err := body.Read(longBuf); err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("%s\n", longBuf)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
