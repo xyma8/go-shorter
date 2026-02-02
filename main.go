5f9b34a30228a1889a5f7c683a572d15a38b6e2a
Никита Семёнов <n.semenov@n.semenov.workstation.bmc-soft.ru>
2026-02-02 15:32:02 +0300

    changes

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+
+	_ "modernc.org/sqlite"
 )
 
 type Message struct {
 	Original_url string
 }
 
+func encodeXOR(id uint) (uint, error) {
+	const MASK uint = 0x2A5B8D3F
+	id = id ^ MASK
+	return id, nil
+}
+
 func encodeBase62(id uint) (string, error) {
 	alphabet := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	binary_result := []byte{0, 0, 0, 0, 0}
+
+	if id >= uintPow(62, 5) {
+		//return "", err
+	}
 
 	for i := 1; id > 0; i++ {
 		mod := id % 62
@@ -24,22 +37,77 @@ func encodeBase62(id uint) (string, error) {
 	return string(binary_result), nil
 }
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello")
-	json_decoder := json.NewDecoder(req.Body)
+func shortURLHandler(w http.ResponseWriter, req *http.Request) {
+	jsonDecoder := json.NewDecoder(req.Body)
 
-	var body_content Message
-	err := json_decoder.Decode(&body_content)
+	var bodyContent Message
+	err := jsonDecoder.Decode(&bodyContent)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(body_content.Original_url)
-
-	encodedID, _ := encodeBase62(125)
+	fmt.Println(bodyContent.Original_url)
+	var id uint = uintPow(62, 5) - 2
+	fmt.Println(id)
+	obfID, _ := encodeXOR(id)
+	encodedID, _ := encodeBase62(obfID)
 	fmt.Println(encodedID)
+
+	io.WriteString(w, encodedID)
+}
+
+func uintPow(base, exponent uint) uint {
+	result := uint(1)
+	for i := uint(0); i < exponent; i++ {
+		result *= base
+	}
+
+	return result
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	db, err := sql.Open("sqlite", "./go-shorter.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Database connected sucessfully")
+
+	//init_database(db)
+
+	var test string
+	errQuery := db.QueryRow(`INSERT INTO urls (original_url, short_url) VALUES ("asdasd", "sdad")`).Scan(&test)
+	if errQuery != nil {
+		log.Fatal(errQuery)
+	}
+	fmt.Println(test)
+
+	errQuery = db.QueryRow("SELECT name FROM urls").Scan(&test)
+	if errQuery != nil {
+		log.Fatal(errQuery)
+	}
+	fmt.Println(test)
+
+	http.HandleFunc("/hello", shortURLHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func init_database(db *sql.DB) {
+	var talbeUrls string
+
+	err := db.QueryRow(`CREATE TABLE urls (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		original_url TEXT NOT NULL,
+		short_url TEXT UNIQUE,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`).Scan(&talbeUrls)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	print("Table 'urls' created")
 }
